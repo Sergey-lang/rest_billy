06ef646a3a8558a94941e3ac2e063954b8199bfb
Siarhei <siarhei.kukharonak@startmatter.com>
2023-11-20 18:07:30 +0300

    add product serializer, model, CRUD

--- billy/models.py
+++ billy/models.py
@@ -1,12 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, verbose_name='First Name', default='')
+    last_name = models.CharField(max_length=50, verbose_name='Last Name', default='')
+    points = models.PositiveIntegerField(verbose_name='Points', default=0)
+    received_points = models.PositiveIntegerField(verbose_name='Received Points', default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name='Title')
-    desc = models.TextField(max_length=500, verbose_name='Description')
+    desc = models.TextField(max_length=500, verbose_name='Description', default='')
     amount = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Amount')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.name}'
+        return f'#{self.pk}-{self.name}'
--- billy/views.py
+++ billy/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class CustomPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
+
+
+class ProductView(APIView):
+    pagination_class = CustomPagination()
+
+    def get(self, request, pk=None):
+        try:
+            if pk:
+                product = Product.objects.get(pk=pk)
+                serializer = ProductSerializer(product)
+                return Response(serializer.data)
+
+            products = Product.objects.all()
+            paginated_queryset = self.pagination_class.paginate_queryset(products, request, self)
+            serializer = ProductSerializer(paginated_queryset, many=True)
+            return self.pagination_class.get_paginated_response(serializer.data)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        if not pk:
+            return Response({'error': 'Method PUT not allowed'})
+        try:
+            product = Product.objects.get(pk=pk)
+
+            serializer = ProductSerializer(product, data=request.data)
+
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk):
+        if not pk:
+            return Response({'error': 'Method DELETE not allowed'})
+        try:
+            product = Product.objects.get(pk=pk)
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_204_NO_CONTENT)
